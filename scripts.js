bffc943ed4e274fe72201a18414de548866f00cf
Carlos Pizarro <kr105@kr105.com>
2023-03-29 13:23:35 -0300

    Add a test to send 100 messages and display response on a grid

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Transport and stream variables
 var transport = null;
 var unreliableWriter = null;
 var unreliableReader = null;
@@ -5,26 +6,32 @@ var stream = null;
 var reliableWriter = null;
 var reliableReader = null;
 
+// Text encoding and decoding utilities
 const encoder = new TextEncoder('utf-8');
 const decoder = new TextDecoder('utf-8');
 
-// Indicators
+// Grid dimensions
+const gridNumRows = 10;
+const gridNumCols = 10;
+
+// Connection status indicator
 const connectionStatus = document.getElementById("connectionStatus");
 
 // Buttons
 const btnConnect = document.getElementById("btnConnect");
 const btnSend = document.getElementById("btnSend");
+const btnStart1 = document.getElementById("btnStart1");
 
-// Radio checks
+// Radio buttons
 const radioBoth = document.getElementById("radioBoth");
 const radioReliable = document.getElementById("radioReliable");
 const radioUnreliable = document.getElementById("radioUnreliable");
 
-// Inputs
+// Input fields
 const txtServerUrl = document.getElementById("txtServerUrl");
 const txtMessage = document.getElementById("txtMessage");
 
-// Text areas
+// Log text areas
 const txtMainLog = document.getElementById("txtMainLog");
 const txtReliableLog = document.getElementById("txtReliableLog");
 const txtUnreliableLog = document.getElementById("txtUnreliableLog");
@@ -56,9 +63,7 @@ function updateConnectionStatus(status) {
     }
 }
 
-async function send() {
-    const msg = txtMessage.value;
-
+async function sendMsg(msg) {
     if (msg == '') {
         logError(txtMainLog, 'Nothing to send');
         return;
@@ -80,6 +85,7 @@ async function send() {
     await Promise.allSettled(sendingPromises);
 }
 
+// Reliable message receiving loop
 async function reliableLoop() {
     while (true) {
         const { value, done } = await reliableReader.read();
@@ -93,6 +99,7 @@ async function reliableLoop() {
     }
 }
 
+// Unreliable message receiving loop
 async function unreliableLoop() {
     while (true) {
         const { value, done } = await unreliableReader.read();
@@ -103,6 +110,70 @@ async function unreliableLoop() {
 
         const msg = decoder.decode(value);
         log(txtUnreliableLog, `[RECV] ${msg}`);
+
+        // Check if the message is a response for test1
+        const test1Pattern = /^test1-(\d{1,2}|100)$/;
+        if (test1Pattern.test(msg) === true) {
+            const number = msg.replace("test1-", "");
+
+            const cell = document.getElementById(`cell-${number}`);
+            const char = document.getElementById(`char-${number}`);
+
+            // Change background color
+            cell.classList.remove("bg-gray-200");
+            cell.classList.add("bg-green-500");
+        }
+    }
+}
+
+// Draw the grid for the test
+function drawGrid() {
+    const grid = document.getElementById('grid');
+
+    for (let row = 0; row < gridNumRows; row++) {
+        for (let col = 0; col < gridNumCols; col++) {
+            const cell = document.createElement('div');
+            const cellId = row * gridNumRows + col + 1;
+            cell.id = `cell-${cellId}`;
+            cell.classList.add(
+                'cell',
+                'w-full',
+                'h-full',
+                'text-center',
+                'flex',
+                'items-center',
+                'justify-center',
+                'border',
+                'border-gray-300',
+                'cursor-pointer',
+                'relative'
+            );
+
+            const char = document.createElement('span');
+            char.id = `char-${cellId}`;
+            char.classList.add('absolute', 'top-1/2', 'left-1/2', 'transform', '-translate-x-1/2', '-translate-y-1/2', 'whitespace-nowrap', 'max-w-full', 'overflow-hidden');
+
+            cell.appendChild(char);
+            grid.appendChild(cell);
+        }
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function startTest1() {
+    if (radioUnreliable.checked === false) {
+        log(txtMainLog, "The Test1 works only with unreliable connection");
+        return;
+    }
+
+    for (let cellId = 1; cellId <= (gridNumCols * gridNumRows); cellId++) {
+        const cell = document.getElementById(`cell-${cellId}`);
+        sendMsg(`test1-${cellId}`);
+        cell.classList.add("bg-gray-200");
+        await sleep(100);
     }
 }
 
@@ -140,6 +211,8 @@ async function connect() {
     updateConnectionStatus("connected");
 
     btnSend.disabled = false;
+    btnStart1.disabled = false;
+    btnConnect.disabled = true;
 
     // Get streams for unreliable data transmission
     unreliableWriter = transport.datagrams.writable.getWriter();
@@ -153,9 +226,6 @@ async function connect() {
     reliableWriter = stream.writable.getWriter();
     reliableReader = stream.readable.getReader();
 
-    console.log(reliableWriter);
-    console.log(reliableReader);
-
     log(txtReliableLog, 'Initialized reader and writer');
 
     // Execute both receiving loops at the same time
@@ -163,6 +233,14 @@ async function connect() {
     await Promise.all(readers);
 }
 
+// Draw the test grid
+drawGrid();
+
+// Event listeners
 btnConnect.addEventListener("click", connect);
-btnSend.addEventListener("click", send);
+btnSend.addEventListener("click", () => sendMsg(txtMessage.value));
+btnStart1.addEventListener("click", startTest1);
+
+// Buttons initial status
 btnSend.disabled = true;
+btnStart1.disabled = true;
